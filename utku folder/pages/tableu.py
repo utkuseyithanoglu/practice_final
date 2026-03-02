bc2af545d2a719121e8492ff20278894967095b8
utku seyıthanoglu <utkuseyithanoglu@Mac-598.lan>
2026-03-01 23:28:36 -0500

    fixed_tableu

--- utku folder/pages/tableu.py
+++ utku folder/pages/tableu.py
@@ -1,8 +1,8 @@
 import streamlit as st
+st.set_page_config(page_title="NYC EMS Predictor", page_icon="🚑", layout="wide")
 st.markdown("## 📊 Demand & Response Intelligence Dashboard")
 st.markdown("This dashboard helps forecast emergency demand and response performance across NYC boroughs.")
 st.divider()
-st.set_page_config(page_title="NYC EMS Predictor", page_icon="🚑", layout="wide")
 st.sidebar.header("NYC EMS Call & Response Time Predictor")
 tableau_url = "https://public.tableau.com/views/predictcallsbyborough/Dashboard2?:showVizHome=no"
 
